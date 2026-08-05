b1148aef45ea4a767ab6c2862246ecfc8d8cd21e
Guilherme Rosa <seu-email-do-github@exemplo.com>
2026-08-05 14:59:19 -0300

    Adiciona criacao automatica de usuarios iniciais

--- src/main/java/br/com/valinostech/ValinostechgamersApplication.java
+++ src/main/java/br/com/valinostech/ValinostechgamersApplication.java
@@ -1,7 +1,12 @@
 package br.com.valinostech;
 
+import br.com.valinostech.model.Usuario;
+import br.com.valinostech.repository.UsuarioRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class ValinostechgamersApplication {
@@ -10,4 +15,36 @@ public class ValinostechgamersApplication {
         SpringApplication.run(ValinostechgamersApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner criarUsuariosIniciais(UsuarioRepository usuarioRepository, PasswordEncoder passwordEncoder) {
+        return args -> {
+            // 1. Cria o Administrador caso não exista
+            String emailAdmin = "guivalino@gmail.com";
+            if (usuarioRepository.findByEmail(emailAdmin) == null) {
+                Usuario admin = new Usuario();
+                admin.setNome("Guilherme Rabeca Valino Rosa");
+                admin.setCpf("431.076.238-73");
+                admin.setEmail(emailAdmin);
+                admin.setSenha(passwordEncoder.encode("Guivalino2019@"));
+                admin.setTipo("ADMIN");
+                
+                usuarioRepository.save(admin);
+                System.out.println(">>> USUÁRIO ADMIN CRIADO COM SUCESSO!");
+            }
+
+            // 2. Cria o Cliente de Teste caso não exista
+            String emailCliente = "cliente@valinos.com";
+            if (usuarioRepository.findByEmail(emailCliente) == null) {
+                Usuario cliente = new Usuario();
+                cliente.setNome("Cliente Teste");
+                cliente.setCpf("123.456.789-00");
+                cliente.setEmail(emailCliente);
+                cliente.setSenha(passwordEncoder.encode("123456"));
+                cliente.setTipo("CLIENTE");
+                
+                usuarioRepository.save(cliente);
+                System.out.println(">>> CLIENTE DE TESTE CRIADO COM SUCESSO!");
+            }
+        };
+    }
 }
